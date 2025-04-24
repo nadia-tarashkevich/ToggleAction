9429884bb2a603ed94da86cecc7ad9d0f9d21a6a
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2025-04-24 12:05:27 -0700

    Initial commit

---
+++ src/main/Java/com/frr244/toggleaction/ExampleMenuAction.java
@@ -0,0 +1,30 @@
+package com.frr244.toggleaction;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.project.DumbAware;
+import org.jetbrains.annotations.NotNull;
+
+public class ExampleMenuAction extends ToggleAction implements DumbAware {
+
+    protected boolean selected = false;
+
+    public ExampleMenuAction() {
+    }
+
+    public ExampleMenuAction(AnActionEvent e) {
+		super("Some Toggle", "A very fancy description", AllIcons.Toolwindows.ToolWindowChanges);
+        setSelected(e, ! isSelected(e));
+	}
+
+    @Override
+    public boolean isSelected(@NotNull AnActionEvent anActionEvent) {
+        return selected;
+    }
+
+    @Override
+    public void setSelected(@NotNull AnActionEvent anActionEvent, boolean b) {
+        selected = b;
+    }
+
+}
